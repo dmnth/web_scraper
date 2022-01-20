6eb49d4d4bfcb0e2571f58a73cf5b97997b954cd
thdmn <thdmn@localhost.localdomain>
2022-01-20 21:46:52 +0200

    vessels spider

---
+++ vessels/vessels/spiders/vessel_spider.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import scrapy
+import re
+
+class VesselSpider(scrapy.Spider):
+
+    name = 'vessels'
+    start_urls = ['https://www.vesselfinder.com/vessels']
+
+    def parse(self, response):
+        for vessel in response.css('tr'):
+            vessel_name = vessel.css('div.slna::text').get()
+            vessel_type = vessel.css('div.slty::text').get()
+            year_built = vessel.css('td.v3::text').get()
+            size = vessel.css('td.v6::text').get()
+            link = vessel.css('td a.ship-link::attr(href)').get()
+            imo_pattern = re.compile('(?<=IMO-)[0-9]+)')
+            msi_pattern = re.compile('(?<MSI-)[0-9]+)')
+            imo = pattern.search(link)
+            msi = pattern.search(link)
+            '''
+            vessel_link = response.urljoin(link)
+            if vessel_link is not None:
+                yield response.follow(vessel_link, callback=self.parse_imo)
+            '''
+            result = {
+                    'name': vessel_name,
+                    'type': vessel_type,
+                    'year built': year_built,
+                    'size': size,
+                    'imo': imo.group(),
+                    'msi': msi.group(),
+                    }
+            yield result 
+
+    def parse_imo(self, response):
+        def extract_imo(query):
+            result = response.css(query).get().split('/')[0].rstrip()
+            if result:
+                return result
+
+        yield {
+                'imo': extract_imo('td.v3.v3np::text'),
+                }
